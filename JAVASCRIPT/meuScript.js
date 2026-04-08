9b665ee2604f16193555e80fa787c216b0b101a0
ArtOliv <arthurcvo1@gmail.com>
2026-04-08 01:41:34 -0300

    Alterado a lógica de reinício do jogo e centralização da forca

--- JAVASCRIPT/meuScript.js
+++ JAVASCRIPT/meuScript.js
@@ -9,36 +9,53 @@ const botao_envia = document.getElementById("botao_envia")
 const botao_reinicia = document.getElementById("botao_reinicia")
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
+const offset = (canvas.width - 140) / 2;
 
 let erros = 0
 
 function mostrarGameOver(mensagem) {
     Toastify({
         text: mensagem,
-        duration: 3000,
+        duration: -1,
         gravity: "middle",
         position: "center",
-        backgroundColor: "red",
+        style: {
+            background: "red",
+            fontSize: "20px",       // Tamanho da fonte
+            padding: "20px 40px",   // Tamanho do retângulo
+            borderRadius: "20px"    // Arredondamento
+        },
     }).showToast();
 }
 
 function mostrarVitoria(mensagem) {
     Toastify({
         text: mensagem,
-        duration: 3000,
+        duration: -1,
         gravity: "middle",
         position: "center",
-        backgroundColor: "green",
+        style: {
+            background: "green",
+            fontSize: "20px",       // Tamanho da fonte
+            padding: "20px 40px",   // Tamanho do retângulo
+            borderRadius: "20px"    // Arredondamento
+        },
     }).showToast();
 }
 
 function efeitoVitoria() {
     if (typeof confetti !== 'undefined') {
         confetti({
-            angle: 90,
+            angle: 60,
             spread: 200,
-            particleCount: 500,
-            origin: { x: 0 }
+            particleCount: 250,
+            origin: { x: 0, y: 1}
+        });
+        confetti({
+            angle: 120,
+            spread: 200,
+            particleCount: 250,
+            origin: { x: 1, y: 1}
         });
     } else {
         console.error("Biblioteca confetti não carregada");
@@ -61,6 +78,12 @@ function efeitoDerrota() {
         }
     };
     flash();
+}
+
+function encerrarInterface(){
+    input_palavra.style.display = "none";
+    botao_envia.style.display = "none";
+    botao_reinicia.style.display = "inline-block";
 }
 
 function letra_errada() {
@@ -107,6 +130,10 @@ input_palavra.addEventListener("keydown", function(event) {
     }
 });
 
+botao_reinicia.addEventListener("click", function(){
+    location.reload();
+});
+
 botao_envia.addEventListener("click", function() {
     const letra = input_palavra.value;
     if (palavra_sorteada.includes(letra)) {        
@@ -125,18 +152,17 @@ botao_envia.addEventListener("click", function() {
     function verificar_vitoria(){
     const span = document.getElementById("palavra")
         if (!span.textContent.includes("_")) {
-            mostrarVitoria("Parabéns! Você venceu!");
+            mostrarVitoria("Parabéns! Você venceu");
             efeitoVitoria();
-            setTimeout(() => location.reload(), 3000);
-            
+            encerrarInterface()
         }
     }
 
     function verificar_derrota(){
-        if (erros > 7) {
+        if (erros >= 7) {
            mostrarGameOver("Game Over! A palavra era: " + palavra_sorteada);
             efeitoDerrota();
-            setTimeout(() => location.reload(), 1000);
+            encerrarInterface()
         }
     }
 
@@ -147,26 +173,26 @@ botao_envia.addEventListener("click", function() {
 
     //base
     ctx.beginPath();
-    ctx.moveTo(10, 250);
-    ctx.lineTo(150, 250);
+    ctx.moveTo(10 + offset, 250);
+    ctx.lineTo(150 + offset, 250);
     ctx.stroke();
 
     //poste
     ctx.beginPath();
-    ctx.moveTo(30, 250);
-    ctx.lineTo(30, 10);
+    ctx.moveTo(30 + offset, 250);
+    ctx.lineTo(30 + offset, 10);
     ctx.stroke();
 
     //barra horizontal
     ctx.beginPath();
-    ctx.moveTo(30, 10);
-    ctx.lineTo(100, 10);
+    ctx.moveTo(30 + offset, 10);
+    ctx.lineTo(100 + offset, 10);
     ctx.stroke();
 
     //corda
     ctx.beginPath();
-    ctx.moveTo(100, 10);
-    ctx.lineTo(100, 70);
+    ctx.moveTo(100 + offset, 10);
+    ctx.lineTo(100 + offset, 70);
     ctx.stroke();
     }
 
@@ -174,73 +200,66 @@ botao_envia.addEventListener("click", function() {
         ctx.strokeStyle = "#e2e8f0";
         ctx.lineWidth = 3;
         switch(erros) {
-        case 1: //cabeça
-            ctx.beginPath();
-            ctx.arc(100, 90, 20, 0, Math.PI * 2);
-            ctx.stroke();
-            break;
+            case 1: //cabeça
+                ctx.beginPath();
+                ctx.arc(100 + offset, 90, 20, 0, Math.PI * 2);
+                ctx.stroke();
+                break;
 
-        case 2: //corpo
-            ctx.beginPath();
-            ctx.moveTo(100, 110);
-            ctx.lineTo(100, 160);
-            ctx.stroke();
-            break;
+            case 2: //corpo
+                ctx.beginPath();
+                ctx.moveTo(100 + offset, 110);
+                ctx.lineTo(100 + offset, 160);
+                ctx.stroke();
+                break;
 
-        case 3: 
-            ctx.beginPath();
-            ctx.moveTo(100, 120);
-            ctx.lineTo(80, 150);
-            ctx.stroke();
-            break;
+            case 3: 
+                ctx.beginPath();
+                ctx.moveTo(100 + offset, 120);
+                ctx.lineTo(80 + offset, 150);
+                ctx.stroke();
+                break;
 
-        case 4:
-            ctx.beginPath();
-            ctx.moveTo(100, 120);
-            ctx.lineTo(120, 150);
-            ctx.stroke();
-            break;
+            case 4:
+                ctx.beginPath();
+                ctx.moveTo(100 + offset, 120);
+                ctx.lineTo(120 + offset, 150);
+                ctx.stroke();
+                break;
 
-        case 5:
-            ctx.beginPath();
-            ctx.moveTo(100, 160);
-            ctx.lineTo(80, 190);
-            ctx.stroke();
-            break;
-        
-        case 6:
-            ctx.beginPath();
-            ctx.moveTo(100, 160);
-            ctx.lineTo(120, 190);
-            ctx.stroke();
-            break;
+            case 5:
+                ctx.beginPath();
+                ctx.moveTo(100 + offset, 160);
+                ctx.lineTo(80 + offset, 190);
+                ctx.stroke();
+                break;
+            
+            case 6:
+                ctx.beginPath();
+                ctx.moveTo(100 + offset, 160);
+                ctx.lineTo(120 + offset, 190);
+                ctx.stroke();
+                break;
 
-        case 7:
-            ctx.beginPath();
-            ctx.moveTo(90, 85);
-            ctx.lineTo(95, 90);
-            ctx.stroke();
-            ctx.moveTo(95, 85);
-            ctx.lineTo(90, 90);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.moveTo(105, 85);
-            ctx.lineTo(110, 90);
-            ctx.stroke();
-            ctx.moveTo(110, 85);
-            ctx.lineTo(105, 90);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.moveTo(95, 100);
-            ctx.lineTo(105, 100);
-            ctx.stroke();
-            break;
-
+            case 7:
+                ctx.beginPath();
+                ctx.moveTo(90 + offset, 85);
+                ctx.lineTo(95 + offset, 90);
+                ctx.stroke();
+                ctx.moveTo(95 + offset, 85);
+                ctx.lineTo(90 + offset, 90);
+                ctx.stroke();
+                ctx.beginPath();
+                ctx.moveTo(105 + offset, 85);
+                ctx.lineTo(110 + offset, 90);
+                ctx.stroke();
+                ctx.moveTo(110 + offset, 85);
+                ctx.lineTo(105 + offset, 90);
+                ctx.stroke();
+                ctx.beginPath();
+                ctx.moveTo(95 + offset, 100);
+                ctx.lineTo(105 + offset, 100);
+                ctx.stroke();
+                break;
     }
 }
-
-botao_reinicia.addEventListener("click", function(){
-    exibir_palavra();
-    const span_letras_erradas = document.getElementById("letras_erradas")
-    span_letras_erradas.textContent = ""
-});
